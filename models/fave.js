4f14ea42dcd25291b149f52e4fe0eff6969b66a1
Jose Morales <josemorales@dhcp-v072-175.mobile.uci.edu>
2017-10-02 21:01:29 -0700

    update

---
+++ models/fave.js
@@ -0,0 +1,9 @@
+// create tables for favorited restaurants
+
+module.exports = function(sequelize, DataTypes) {
+  var Fave = sequelize.define("Fave", {
+    // restaurant details
+
+  });
+  return Fave;
+};
---
+++ public/js/view.js
@@ -0,0 +1,85 @@
+// Set Variables
+var title = localStorage.getItem("title");
+var description = localStorage.getItem("description");
+var eventStart = localStorage.getItem("eventstart");
+var eventEnd = localStorage.getItem("eventend");
+var logo = localStorage.getItem("logo");
+var venue = localStorage.getItem("venue");
+var latitude = Number(localStorage.getItem("latitude"));
+var longitude = Number(localStorage.getItem("longitude"));
+var address = localStorage.getItem("address");
+var organizer = localStorage.getItem("organizer");
+var bigLogo = localStorage.getItem("biglogo");
+
+// Variable for Yelp API
+var q = "restaurants";
+
+// Populate event page HTML with object values in local storage
+function populate() {
+	$("#event-heading").html(title);
+	$("#event-date").html("Start: " + eventStart + "</br>" + "End: " + eventEnd);
+	$("#event-location").html(venue + "<br>" + address);
+	$("#event-organizer").html(organizer);
+	$("#event-description").html(description);
+	if (bigLogo != "undefined") {
+		$("#main-image").attr("src", bigLogo);
+	}
+}
+
+// Yelp APi & Node.js
+$(document).ready(function() {
+	var queryURL = 	"https://pure-savannah-62932.herokuapp.com/yelp/?q=" + q + "&location=" + address + "&radius=5mi&open_now=true";
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		var yelpResults = response.jsonBody.businesses;
+		console.log(yelpResults);
+		for (var i = 0; i < yelpResults.length; i++) {
+			var distanceMiles = Math.round((yelpResults[i].distance * 0.000621371192)*100)/100;
+			console.log(yelpResults[i].location);
+			var yelpDiv = $("<div class='items'>");
+			var yelpData =  "<div class='image-resize-div'><img class='thumbnail image-resize' src='" + yelpResults[i].image_url + "' width='300'></div><h5><b>" + yelpResults[i].name + "</b></h5><p><b>Price:</b> " + yelpResults[i].price + "<br/><b>Rating:</b> " + yelpResults[i].rating + "/5<br/><b>Miles Away:</b> " + distanceMiles + "<br/><b>Phone:</b> " + yelpResults[i].display_phone +"</p><a href='" + yelpResults[i].url + "' class='button small expanded hollow yelp-link'>Take Me There</a>";
+			// + yelpResults[i].display_phone + "</td><td>" + yelpResults[i].location.display_address + "</td><td>" + distanceMiles +"</td>";
+			yelpDiv.append(yelpData);
+			$(".results").append(yelpDiv);
+		};
+	});
+});
+
+// Google Map
+function initMap() {
+	var origin = {lat: latitude, lng: longitude};
+	var infowindow = new google.maps.InfoWindow();
+	var map = new google.maps.Map(document.getElementById('map-div'), {
+		zoom: 15,
+		center: origin
+	});
+		var service = new google.maps.places.PlacesService(map);
+		var marker = new google.maps.Marker({
+		position: origin,
+		map: map,
+		title: address
+	});
+	marker.addListener('click', function() {
+		map.setZoom(17);
+		map.setCenter(marker.getPosition());
+		infowindow.setContent('<div><strong>Venue: ' + venue  + '</strong></br>' + address + '</strong></br>')
+    		infowindow.open(map, this);
+	});
+	service.getDetails({
+		placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+	}, function(place, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			var marker = new google.maps.Marker({
+				map: map,
+				position: place.geometry.location
+			});
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id + '<br>' + place.formatted_address + '</div>');
+				infowindow.open(map, this);
+			});
+		}
+	});
+};

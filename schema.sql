a8efa07ada854e411dc144971285964c6fd81470
jayyymorales <jrmorales@ymail.com>
2017-10-07 15:00:31 -0700

    server update

--- schema.sql
+++
@@ -1,5 +0,0 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS tenderfood;
--- Creates the "todolist" database --
-CREATE DATABASE tenderfood;
- 
--- server.js
+++ server.js
@@ -15,6 +15,9 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Requiring our models for syncing
+var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -29,11 +32,14 @@ app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-// require("./app/routes/html-routes.js")(app);
+require("./routes/fave-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-// Starts the server to begin listening
+// Syncing our sequelize models and then starting our Express app
 // =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });

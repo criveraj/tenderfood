293261a8bcc8ae0661917e7875511c1af3e184b6
jayyymorales <jrmorales@ymail.com>
2017-10-07 18:27:56 -0700

    fixed server issue

--- server.js
+++ server.js
@@ -6,20 +6,30 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport = require('passport');
+var session = require('express-session');
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8070;
+var PORT = process.env.PORT || 8000;
 
 // Requiring our models for syncing
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Causing issues with localization
+
+// app.use(session({ secret: 'tendr',resave: true, saveUninitialized:true})); // session secret
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Static directory
 app.use(express.static("public"));
